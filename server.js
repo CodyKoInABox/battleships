16229cbc00df702ca06c72d4af491bd87d0a3850
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-27 17:27:54 -0300

    socketio user connection setup

--- server.js
+++ server.js
@@ -12,5 +12,31 @@ const PORT = process.env.PORT || 8080
 app.use(express.static('./public'))
 
 
+io.on('connection', (socket) => {
+
+    socket.on('joinRoom', (roomName) =>{
+        socket.join(roomName)
+
+        try{
+            socket.to(roomName).emit('userCount' ,{'content': 'Someone joined', 'userCount': io.sockets.adapter.rooms.get(roomName).size})
+        }catch(e){
+            socket.broadcast.to(roomName).emit('message', {'message' : 'Error while calculating the user count', 'error' : e})
+        }
+
+        socket.on('disconnect', () => {
+            //io.to(roomName).emit('message', 'Disconnection detected!')
+            try{
+                socket.broadcast.to(roomName).emit('userCount' ,{'content': 'Someone disconnect', 'userCount': io.sockets.adapter.rooms.get(roomName).size})
+            }catch(e){
+                socket.broadcast.to(roomName).emit('message', {'message' : 'Error while calculating the user count', 'error' : e})
+            }finally{
+            }
+        })
+    })
+
+    socket.emit('message', 'Hello!')
+})
+
+
 
 server.listen(PORT, () => console.log(`Live on port ${PORT}`))
