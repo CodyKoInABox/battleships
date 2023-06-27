3d375f77ee7a743d5f4174a00774def95afeacce
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-27 19:05:53 -0300

    fill board grid with divs

--- helper.js
+++ helper.js
@@ -1,2 +1,9 @@
 // this file is used to print large amounts of text without someone having to manually type all of it
 // and other useful things
+
+
+for(let i=0; i<=9; i++){
+    for(let j=0; j<=9; j++){
+        console.log('<div clas="mapTile" id="'+ i.toString() + j.toString() + 'r' + '"></div>');
+    }
+}
