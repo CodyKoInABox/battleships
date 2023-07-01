8d86f7f604c731ba9111383f0aa237837a2ac57d
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-06-30 21:19:27 -0300

    separate functions by file

--- public/play/play.js
+++ public/play/play.js
@@ -1,47 +1,6 @@
 
-
-
-function getNeighbors(startCoord, amount, isVertical){
-
-    let startCoordVertical = startCoord[0]
-    let startCoordHorizontal = startCoord[1]
-
-    let neighbors = []
-
-
-    if(isVertical){
-        // get vertical neighbors
-    
-        for(let i = 1; i <= amount; i++){
-
-            let currentNeighbor = ''
-
-            let temp = parseInt(startCoordVertical) + i
-
-            currentNeighbor = currentNeighbor + temp.toString() + startCoordHorizontal
-
-            neighbors.push(currentNeighbor)
-        }
+import getNeighbors from "./functions/getNeighbors.js"
 
 
 
-    }else{
-        // get horizontal neighbors
 
-        for(let i = 1; i <= amount; i++){
-
-            let currentNeighbor = ''
-
-            let temp = parseInt(startCoordHorizontal) + i
-
-            currentNeighbor = currentNeighbor + startCoordVertical + temp.toString()
-
-            neighbors.push(currentNeighbor)
-        }
-
-
-    }
-   
-
-    return neighbors
-}
